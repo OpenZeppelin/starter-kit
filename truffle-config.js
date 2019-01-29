05c62bcedc65e5faa90125170c6ff42e41894f29
Ramon Recuero <ramon@dapis.io>
2019-01-29 12:14:00 -0800

    Config for all networks with Infura

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,45 @@
 const path = require("path");
+require('dotenv').config();
+const mnemonic = process.env.MNENOMIC;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "client/src/contracts")
+  contracts_build_directory: path.join(__dirname, "client/src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/v3/' + process.env.INFURA_API_KEY)
+      },
+      network_id: '3',
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + process.env.INFURA_API_KEY)
+      },
+      network_id: '42',
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 4,
+      gas: 3000000,
+      gasPrice: 10000000000
+    },
+    // main ethereum network(mainnet)
+    main: {
+      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/" + process.env.INFURA_API_KEY),
+      network_id: 1,
+      gas: 3000000,
+      gasPrice: 10000000000
+    }
+  }
 };
