c48f739ba8260c0444f425465464566f9cb78d33
Ramon Recuero <ramon@dapis.io>
2019-01-30 11:51:58 -0800

    Updates to web 3 beta 37

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import Counter from "./contracts/Counter.json";
 import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
@@ -11,21 +11,24 @@ class App extends Component {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
+      console.log('web3.version', web3.version);
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      const deployedNetwork = Counter.networks[networkId];
+      if (deployedNetwork) {
+        const instance = new web3.eth.Contract(
+          Counter.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+        console.log('instance', instance.methods);
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        this.setState({ web3, accounts, contract: instance }, this.getCount);
+      } else {
+        this.setState({ web3, accounts });
+      }
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,22 +38,42 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
+  getCount = async () => {
+    const { contract } = this.state;
+    console.log('contract.methods', contract.methods.getCounter());
+    // // Get the value from the contract to prove it worked.
+    // const response = contract.methods.getCounter().call();
+    // console.log('response', response);
+    // // Update state with the result.
+    // this.setState({ storageValue: response });
+  };
+
+  increaseCount = async (number) => {
     const { accounts, contract } = this.state;
+    await contract.methods.increaseCounter(number).send({ from: accounts[0] });
+    this.getCount();
+  };
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  decreaseCount = async (number) => {
+    const { accounts, contract } = this.state;
+    await contract.methods.decreaseCounter(number).send({ from: accounts[0] });
+    this.getCount();
   };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <div>
+          Loading Web3, accounts, and contract...
+        </div>
+      );
+    }
+    if (!this.state.contract) {
+      return (
+        <div>
+          Wrong network
+        </div>
+      )
     }
     return (
       <div className="App">
