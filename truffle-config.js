1b2b63c12cdd99c1f378ff96a0f3f9c4e56f4532
Ramon Recuero <ramon@dapis.io>
2019-02-05 15:12:11 -0800

    Adds hdwallet provider to the dependencies

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 require('dotenv').config();
 const mnemonic = process.env.MNENOMIC;
+const HDWalletProvider = require("truffle-hdwallet-provider");
 // Create your own key for Production environments (https://infura.io/)
 const INFURA_ID = 'd6760e62b67f4937ba1ea2691046f06d';
 
