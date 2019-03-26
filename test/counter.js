f988fee8c350c0e9f89f93dca7d685b8a8d5df25
Igor Yalovoy <igor@yalovoi.net>
2019-03-26 20:27:25 -0300

    Add Solidity tests

---
+++ test/counter.js
@@ -0,0 +1,23 @@
+import chai from 'chai';
+import {createMockProvider, deployContract, getWallets, solidity} from 'ethereum-waffle';
+import Counter from '../build/Counter';
+
+chai.use(solidity);
+const {expect} = chai;
+
+describe('counter', () => {
+  let provider = createMockProvider();
+  let [wallet, walletTo] = getWallets(provider);
+  const value = 9000;
+  let counter;
+
+  beforeEach(async () => {
+    counter = await deployContract(wallet, Counter);
+  });
+
+  it('should return proper value', async () => {
+    await counter.initialize(value);
+    expect((await counter.getCounter()).toNumber()).to.eq(value);
+  });
+
+});
