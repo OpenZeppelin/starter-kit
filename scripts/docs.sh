1d24d5c48a5e00253c836bb04d66f34f02dbb12f
Ramon Recuero <rrecuero@MBP-de-Ramon.lan>
2019-07-22 14:17:34 -0700

    Renames folder

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -15,10 +15,10 @@ git -C openzeppelin-docs pull -q
 
 if [ "$1" = "build" ]; then
   cd docs
-  env DISABLE_PREPARE_DOCS= node ../openzeppelin-docs/build-local.js
+  env DISABLE_PREPARE_DOCS= node ../docs.openzeppelin.com/build-local.js
 
 elif [ "$1" = "start" ]; then
   npx concurrently \
-    'cd docs; nodemon -e adoc,yml ../openzeppelin-docs/build-local.js' \
-    'http-server -c-1 openzeppelin-docs/build/site'
+    'cd docs; nodemon -e adoc,yml ../docs.openzeppelin.com/build-local.js' \
+    'http-server -c-1 docs.openzeppelin.com/build/site'
 fi
