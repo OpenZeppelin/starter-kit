912178f8d258809dc918c5270fc435cfdd337395
Igor Yalovoy <igor@yalovoi.net>
2019-07-23 21:43:23 +0300

    Support React Hooks

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { fromInjected, fromConnection } from 'openzeppelin-network';
+import React, { useState, useEffect } from 'react';
+import { useWeb3Injected, useWeb3Network } from '@openzeppelin/network';
 import Web3Info from './components/Web3Info/index.js';
 
 import styles from './App.module.scss';
 
 function App() {
+  const injected = useWeb3Injected();
+  const local = useWeb3Network('http://127.0.0.1:8545');
+  const network = useWeb3Network('wss://ropsten.infura.io/ws/v3/95202223388e49f48b423ea50a70e336');
+
   return (
     <div className={styles.App}>
       <br />
       <h1>BUIDL with Starter Kit</h1>
-      <Web3Info title="Injected Web3" web3Factory={() => fromInjected()} />
+      <Web3Info title="Injected Web3" web3Context={injected} />
       <br />
-      <Web3Info title="Local Web3 Node" web3Factory={() => fromConnection('http://127.0.0.1:8545')} />
+      <Web3Info title="Local Web3 Node" web3Context={local} />
       <br />
-      <Web3Info
-        title="Infura Web3"
-        web3Factory={() =>
-          fromConnection('https://ropsten.infura.io/v3/95202223388e49f48b423ea50a70e336', {
-            pollInterval: 10000,
-          })
-        }
-      />
+      <Web3Info title="Infura Web3" web3Context={network} />
     </div>
   );
 }
